dec9ebbf9ba23b6145f9f235d2d8f3633a7e4385
chengshengg <kongchengsheng@gmail.com>
2025-10-21 13:52:01 +0800

    a

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
-testing 123
+function fetchData(callback) {
+    setTimeout(function() {
+        callback("Data fetched");
+    }, 2000);
+}
+
+function handleData(data) {
+    console.log(data);
+}
+
+fetchData(handleData);
+
+function fetchUsers(callback) {
+    setTimeout(function() {
+        callback([{ name: "Ah Beng", age: 25 }, { name: "Ali", age: 22 }]);
+    }, 2000);
+}
+
+function printUsers(users) {
+    users.forEach(function(user) {
+        console.log(`Name: ${user.name}, Age: ${user.age}`);
+    });
+}
+
+fetchUsers(printUsers);
