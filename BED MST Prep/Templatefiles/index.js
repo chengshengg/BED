092a6312f01437f6d29127e040d094f81d18b588
chengshengg <kongchengsheng@gmail.com>
2025-12-08 13:43:48 +0800

    a

--- BED MST Prep/Templatefiles/index.js
+++ BED MST Prep/Templatefiles/index.js
@@ -20,28 +20,3 @@ app.listen(PORT, ()=>{
     console.log(`App listening on ${PORT}`)
 })
 
-// npm init
-// npm install express nodemon mysql2 dotenv
-
-//   "scripts": {
-// "init_tables": "node src/configs/createSchema.js && node src/configs/initTables.js",
-// "start": "node index.js",
-// "dev": "nodemon index.js"
-
-// DELETE NODE MODULES FOLDER
-// all functions need to have 500 server error, good to have more error checking
-if(error){
-console.error("Error ...", error)
-resizeBy.status(500).json({message: "Internal..."})}
-
-// WORD route should always be on top, then blank, then params
-
-res.status(200).json({
-    message: "...",
-    statistics: results
-
-})
-// SELECT diploma, COUNT(*) AS numberofguests
-// AS in sql allows u to change the variable name
-// GROUP BY diploma
-
